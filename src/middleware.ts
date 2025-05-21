074d858ef5de2efc6b3250cb3f4829c390317c92
zdemirgithub <you@example.com>
2025-05-22 00:17:34 +0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ const isPublicRoute = createRouteMatcher([
   '/api/stripe(.*)',
   '/privacy',
   '/terms-of-service',
-  '/api/clerk/webhook(.*)'
+  '/api/webhooks/clerk(.*)'
 ])
 
 export default clerkMiddleware((auth, req) => {
